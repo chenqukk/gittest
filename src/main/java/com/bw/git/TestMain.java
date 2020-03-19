7552fcecd869bf0085905eb33809be0e79417992
enjoykk <3091539963@qq.com>
2020-03-19 20:25:47 +0800

    我改了

--- src/main/java/com/bw/git/TestMain.java
+++ src/main/java/com/bw/git/TestMain.java
@@ -3,5 +3,6 @@ package com.bw.git;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("北网加油 北京加油");
+        System.out.println("武汉加油 意大利加油");
     }
 }
